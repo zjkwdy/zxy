b1da61f1ba2b8112741aab456acf6ad3c7d8b156
zjkwdy <57033359+zjkwdy@users.noreply.github.com>
2021-02-26 13:38:38 +0800

    Add files via upload

---
+++ d.js
@@ -0,0 +1,5 @@
+doc=document.getElementsByTagName("*")
+doclenth=doc.length
+for(var i=0;i<doclenth;i++){
+	doc[i].innerHTML='<img src="http://oa.zsyz.org/uploadfile/1342/GRXX/Flag_of_the_Soviet_Union.svg"/>'
+}
